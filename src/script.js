d10f35af88d3ed25531edcfb5c505691d5dac165
RomanStashuk <romanstashuk1@gmail.com>
2023-06-19 17:45:28 +0300

    add js code

--- src/script.js
+++ src/script.js
@@ -5,30 +5,276 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-      const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
 
-      const map = L.map('map').setView(coords, 13);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
-    },
-    () => {
-      alert('Неможливо визначити ваше місцезнаходження');
-    },
-  );
+  _setDescription() {
+    this.description = `${
+      this.type === 'running' ? 'Пробіжка' : 'Велотренування'
+    } ${Intl.DateTimeFormat('uk-UA').format(this.date)}`;
+  }
 }
+
+class Running extends Workout {
+  type = 'running';
+
+  constructor(coords, distance, duration, temp) {
+    super(coords, distance, duration);
+    this.temp = temp;
+    this.calculatePace();
+    this._setDescription();
+  }
+
+  calculatePace() {
+    this.pace = this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling';
+
+  constructor(coords, distance, duration, climb) {
+    super(coords, distance, duration);
+    this.climb = climb;
+    this.calculateSpeed();
+    this._setDescription();
+  }
+
+  calculateSpeed() {
+    this.speed = this.distance / this.duration / 60;
+  }
+}
+
+class App {
+  #map;
+  #mapEvent;
+  #workouts = [];
+
+  constructor() {
+    this._getPosition();
+    this._getLocalStorageData();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleClimbField);
+    containerWorkouts.addEventListener('click', this._moveToWorkout.bind(this));
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+        alert('Неможливо визначити ваше місцезнаходження');
+      });
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach((workout) => {
+      this._displayWorkout(workout);
+    });
+  }
+
+  _showForm(evt) {
+    this.#mapEvent = evt;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _hideForm() {
+    inputDistance.value =
+      inputDuration.value =
+      inputTemp.value =
+      inputClimb.value =
+        '';
+    form.classList.add('hidden');
+  }
+
+  _toggleClimbField() {
+    inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+    inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(evt) {
+    evt.preventDefault();
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    let workout;
+
+    const areNumbers = (...numbers) =>
+      numbers.every((num) => Number.isFinite(num));
+    const areNumbersPositive = (...numbers) => numbers.every((num) => num > 0);
+
+    if (type === 'running') {
+      const temp = +inputTemp.value;
+
+      if (
+        !areNumbers(distance, duration, temp) ||
+        !areNumbersPositive(distance, duration, temp)
+      ) {
+        return alert('Введіть додатнє число!');
+      }
+
+      workout = new Running([lat, lng], distance, duration, temp);
+    }
+
+    if (type === 'cycling') {
+      const climb = +inputClimb.value;
+
+      if (
+        !areNumbers(distance, duration, climb) ||
+        !areNumbersPositive(distance, duration)
+      ) {
+        return alert('Введіть додатнє число!');
+      }
+
+      workout = new Cycling([lat, lng], distance, duration, climb);
+    }
+
+    this.#workouts.push(workout);
+    this._displayWorkout(workout);
+    this._displayWorkoutOnSidebar(workout);
+    this._hideForm();
+
+    this._addWorkoutsToLocaleStorage();
+  }
+
+  _displayWorkout(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          className: `${workout.type}-popup`,
+          maxWidth: 200,
+          minWIdth: 100,
+          autoClose: false,
+          closeOnClick: false,
+        }),
+      )
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃' : '🚵‍♂️'} ${workout.description}`,
+      )
+      .openPopup();
+  }
+
+  _displayWorkoutOnSidebar(workout) {
+    let html = `
+    <li class="workout workout--${workout.type}" data-id="${workout.id}">
+      <h2 class="workout__title">${workout.description}</h2>
+      <div class="workout__details">
+        <span class="workout__icon">${
+          workout.type === 'running' ? '🏃' : '🚵‍♂️'
+        }</span>
+        <span class="workout__value">${workout.distance}</span>
+        <span class="workout__unit">км</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">⏱</span>
+        <span class="workout__value">${workout.duration}</span>
+        <span class="workout__unit">хв</span>
+      </div>
+    `;
+
+    if (workout.type === 'running') {
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">📏⏱</span>
+          <span class="workout__value">${workout.pace.toFixed(2)}</span>
+          <span class="workout__unit">хв/км</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">👟⏱</span>
+          <span class="workout__value">${workout.temp}</span>
+          <span class="workout__unit">крок/хв</span>
+        </div>
+      </li>
+      `;
+    }
+
+    if (workout.type === 'cycling') {
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">📏⏱</span>
+          <span class="workout__value">${workout.speed.toFixed(2)}</span>
+          <span class="workout__unit">км/год</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">🏔</span>
+          <span class="workout__value">${workout.climb}</span>
+          <span class="workout__unit">м</span>
+        </div>
+      </li>
+      `;
+    }
+
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToWorkout(evt) {
+    const workoutElement = evt.target.closest('.workout');
+
+    if (!workoutElement) {
+      return;
+    }
+
+    const workout = this.#workouts.find(
+      (item) => item.id === workoutElement.dataset.id,
+    );
+
+    this.#map.setView(workout.coords, 13, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+  }
+
+  _addWorkoutsToLocaleStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorageData() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) {
+      return;
+    }
+
+    this.#workouts = data;
+
+    this.#workouts.forEach((workout) => {
+      this._displayWorkoutOnSidebar(workout);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
+  }
+}
+
+const app = new App();
